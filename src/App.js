b46bbc0b4f16f8fe05f049eb0bd8a80036c0c55b
Rajeeradha <127470577+Rajeeradha@users.noreply.github.com>
2023-11-06 21:28:37 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Register from './Components/Register';
 import Signin from './Components/Signin';
 import Home from './Components/Home';
